c0ba6c41195e1b83156b8e0ba960e3f212b3e4d7
nalin <stackoverflowpanic@gmail.com>
2023-11-22 18:43:27 -0500

    chat impl

--- src/llms/openai.py
+++ src/llms/openai.py
@@ -1,23 +1,27 @@
 from .base import LLM
-import openai
+from openai import OpenAI
 
-class Chat(LLM):
+class GPT_3_5(LLM):
     def __init__(self, api_key):
-        openai.api_key = api_key
-        self.messages = [
-            {"role": "system",
-             "content": "You are a intelligent assistant."}
-        ]
+        self.client = OpenAI(api_key = api_key)
+        # self.messages = [
+        #     {"role": "system",
+        #      "content": "You are a intelligent assistant."}
+        # ]
 
     def generate(self, prompt):
-        self.messages.append(
-             {"role": "user",
-              "content": prompt}
-        )
-        response = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=self.messages)
+        # self.messages.append(
+        #      {"role": "user",
+        #       "content": prompt}
+        # )
+        response = self.client.chat.completions.create(model="gpt-3.5-turbo", messages=prompt)
         reply = response.choices[0].message.content
-        self.messages.append(
-             {"role": "assistant",
-              "content": reply}
-        )
+        # self.messages.append(
+        #      {"role": "assistant"¯
+        #       "content": reply}
+        # )
         return reply
+    
+    def stream(self, prompt):
+        for response in self.client.chat.completions.create(model="gpt-3.5-turbo", messages=prompt, stream=True):
+            yield (response.choices[0].delta.content or "")
--- src/ui/pages/Chat.py
+++ src/ui/pages/Chat.py
@@ -1,4 +1,5 @@
 import streamlit as st
+from llms.openai import GPT_3_5
 
 st.markdown(
     """
@@ -13,3 +14,24 @@ characters = ["Base", "Accelerationist"]
 with st.sidebar:
     pick_model = st.selectbox("Model", options=models)
     pick_character = st.selectbox("Character", options=characters)
+    if pick_model in ["GPT-3.5", "GPT-4.0"]:
+        openai_api_key = st.text_input("OpenAI API Key", key="api_key", type="password")
+
+if pick_model == "GPT-3.5":
+    model = GPT_3_5(openai_api_key)
+
+prompt = [{"role": "system", "content": "You are a intelligent assistant."}]
+
+if prompt_input := st.chat_input("Just Chat"):
+    prompt.append({"role": "user", "content": prompt_input})
+
+    with st.chat_message("user"):
+        st.markdown(prompt_input)
+
+    with st.chat_message("assistant"):
+        message_placeholder = st.empty()
+        full_response = ""
+        for reply in model.stream(prompt):
+            full_response += reply
+            message_placeholder.markdown(full_response + " |")
+        message_placeholder.markdown(full_response)
