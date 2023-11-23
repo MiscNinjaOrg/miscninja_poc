b969bfe5901f864806ae8bb2453629a85a18cf85
nalin <stackoverflowpanic@gmail.com>
2023-11-22 23:43:07 -0500

    stateful conversation

--- src/ui/pages/Chat.py
+++ src/ui/pages/Chat.py
@@ -1,5 +1,6 @@
 import streamlit as st
 from llms.openai import GPT_3_5
+from wonderwords import RandomWord
 
 st.markdown(
     """
@@ -8,22 +9,39 @@ st.markdown(
     -------------
 """)
 
+if "conversations" not in st.session_state:
+    st.session_state.conversations = {}
+
 models = ["GPT-3.5", "GPT-4.0"]
 characters = ["Base", "Accelerationist"]
+conversations = list(st.session_state.conversations.keys())[::-1]
 
 with st.sidebar:
     pick_model = st.selectbox("Model", options=models)
     pick_character = st.selectbox("Character", options=characters)
+    pick_conversation = st.selectbox("Conversation", options=conversations)
+    new_conversation = st.button("New Conversation", type="primary")
     if pick_model in ["GPT-3.5", "GPT-4.0"]:
         openai_api_key = st.text_input("OpenAI API Key", key="api_key", type="password")
 
 if pick_model == "GPT-3.5":
     model = GPT_3_5(openai_api_key)
 
-prompt = [{"role": "system", "content": "You are a intelligent assistant."}]
+conversation_name = pick_conversation
+if new_conversation:
+    r = RandomWord()
+    conversation_name = r.word(include_parts_of_speech=["adjectives"]) + " " + r.word(include_parts_of_speech=["noun"])
+    prompt = [{"role": "system", "content": "You are a intelligent assistant."}]
+    st.session_state.conversations[conversation_name] = prompt
+    st.rerun()
+
+if conversation_name:
+    for message in st.session_state.conversations[conversation_name]:
+        with st.chat_message(message['role']):
+            st.markdown(message['content'])
 
 if prompt_input := st.chat_input("Just Chat"):
-    prompt.append({"role": "user", "content": prompt_input})
+    st.session_state.conversations[conversation_name].append({"role": "user", "content": prompt_input})
 
     with st.chat_message("user"):
         st.markdown(prompt_input)
@@ -31,7 +49,8 @@ if prompt_input := st.chat_input("Just Chat"):
     with st.chat_message("assistant"):
         message_placeholder = st.empty()
         full_response = ""
-        for reply in model.stream(prompt):
+        for reply in model.stream(st.session_state.conversations[conversation_name]):
             full_response += reply
             message_placeholder.markdown(full_response + " |")
         message_placeholder.markdown(full_response)
+    st.session_state.conversations[conversation_name].append({"role": "assistant", "content": full_response})
